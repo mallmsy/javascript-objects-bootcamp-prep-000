ecef408b1b9feef31de39c4db4dcecc78a072fba
Mallory Woods <malvwoods@gmail.com>
2019-02-13 14:49:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,11 +1,3 @@
 var playlist = {
   "My Bloody Valentine": 'Sometimes'
 }
-
-function updatePlaylist (playlist, artistName, songTitle) {
- playlist["Phil Ochs"], "Here's to the State of Mississippi";
-}
-
-function removeFromPlaylist (playlist, artistName) {
-  delete playlist.Slowdive;
-}
